a9f8526f7286207d447e44ae069d1b0bcef54029
Your Name <you@example.com>
2019-10-08 18:31:39 +0800

    Edit Index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,39 @@
 
 
 // 錢錢大於一個值，叫乾爹
-let money = 40000;
-let limit = 500000; // 限制值
+// let money = 40000;
+// let limit = 500000; // 限制值
 
+https://github.com/Ntub-Class/homework-if-N1086711
+
+console.log('*'.repeat(50));
+
+let money = 44444444000;
+let limit = 500000;
+
+if (money > limit) {
+    console.log('叫乾爹');
+} else {
+    console.log('什麼都不做');
+};
+console.log('*'.repeat(50));
+
+let a:string = 'I love you three thousand.';
+console.log(a.replace('three','ten'));
+
+console.log('*'.repeat(50));
+
+let b:string = 'Did something go wrong';
+console.log(b.concat(' or did something go right,',' Steve Rogers?'));
+
+console.log('*'.repeat(50));
+
+let c: number = 3.14159;
+console.log(c.toFixed(2));
+
+console.log('*'.repeat(50));
+
+let d: number = 10000000;
+console.log(d.toLocaleString());
+
+console.log('*'.repeat(50));
